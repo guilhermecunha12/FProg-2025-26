1d5ae93620efa07979cc345cb022b4712d67d339
Guilherme Cunha <jguilhermecunha@gmail.com>
2025-12-26 02:22:21 +0000

    Updated validate_template.py file

--- Computational_Thinking/validate_template.py
+++ Computational_Thinking/validate_template.py
@@ -4,43 +4,7 @@ def validate(value, rules): # rules is a list of functions
             return False # if just one rule fails, then the value is not valid 
     return True
 
-def has_symbol(value):
-    return '@' in value
-
-def has_username(value):
-    return len(value.split('@')[0]) > 0
-
-def has_valid_domain(value):
-    return '.' in value.split('@')[1] if '@' in value else False
-
-def is_number(value):
-    try:
-        float(value)
-        return True
-    except ValueError:
-        return False
-
-def in_grade_range(value):
-    try:
-        grade = float(value)
-        return 0 <= grade <= 20
-    except ValueError:
-        return False
-
-def has_capital_letters(value):
-    return any(char.isupper() for char in value)
-
-def has_lowercase_letters(value):
-    return any(char.islower() for char in value)
-
-def has_symbols(value):
-    return any(char in '!@#$%^&*()_+-=[]{}|;:,.<>?' for char in value)
-
-def has_digits(value):
-    return any(char.isdigit() for char in value)
-
-
-# Examples of use:
+# Examples of use (functions should be implemented for this to work)
 
 email = input()
 print(validate(email, [has_symbol, has_username, has_valid_domain]))
